1cbfa4d56280be33f824b369cae53ee9ef91554e
Oleh8888 <primaoleg1998@gmail.com>
2020-10-20 14:29:55 +0300

    New

--- project.js
+++ project.js
@@ -1,14 +1,12 @@
-progress {
-    width: 30%;
+tit.onclick = function() {
+    let start = Date.now();
+
+    let timer = setInterval(function() {
+      let timePassed = Date.now() - start;
+
+      train.style.left = timePassed / 5 + 'px';
+
+      if (timePassed > 2000) clearInterval(timer);
+
+    }, 20);
   }
-  elem.onclick = function() {
-    animate({
-      duration: 1000,
-      timing: function(timeFraction) {
-        return timeFraction;
-      },
-      draw: function(progress) {
-        elem.style.width = progress * 100 + '%';
-      }
-    });
-  };

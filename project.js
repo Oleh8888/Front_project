81e8d6b234c0db70d4ea0b7fa5f7ee5748c9c8d8
Oleh8888 <primaoleg1998@gmail.com>
2020-12-21 18:44:52 +0200

    scroll ficha

--- project.js
+++ project.js
@@ -1,5 +1,80 @@
-console.log(
-  setTimeout(textApear, 3000)
-  function textApear() {
-    document.getElementById("moveTitle").innerHTML = "Мы древний кофейный культ.";
-);
+
+
+
+$('#cursorNext').click(function scrollToNextItem() {
+				const scroller = document.getElementById('scroller');
+       		    const itemWidth = document.getElementById('block').clientWidth;
+
+            if(scroller.scrollLeft < (scroller.scrollWidth - itemWidth))
+            	
+                // The scroll position is not at the beginning of last item
+                scroller.scrollBy({left: itemWidth, top: 0, behavior:'smooth'});
+            else
+                // Last item reached. Go back to first item by setting scroll position to 0
+                scroller.scrollTo({left: 0, top: 0, behavior:'smooth'});
+        });
+
+$('#cursorPrev').click(function scrollToNextItem() {
+				const scroller = document.getElementById('scroller');
+       		    const itemWidth = document.getElementById('block').clientWidth;
+            if(scroller.scrollLeft < (scroller.scrollWidth - itemWidth))
+                // The scroll position is not at the beginning of last item
+                scroller.scrollBy({left: itemWidth, top: 0, behavior:'smooth'});
+            else
+                // Last item reached. Go back to first item by setting scroll position to 0
+                scroller.scrollTo({left: 0, top: 0, behavior:'smooth'});
+        });
+
+$('#cursorNext2').click(function scrollToNextItem() {
+				const scroller = document.getElementById('scroller2');
+       		    const itemWidth = document.getElementById('block2').clientWidth;
+
+            if(scroller.scrollLeft < (scroller.scrollWidth - itemWidth))
+            	
+                // The scroll position is not at the beginning of last item
+                scroller.scrollBy({left: itemWidth, top: 0, behavior:'smooth'});
+            else
+                // Last item reached. Go back to first item by setting scroll position to 0
+                scroller.scrollTo({left: 0, top: 0, behavior:'smooth'});
+        });
+
+$('#cursorPrev2').click(function scrollToNextItem() {
+				const scroller = document.getElementById('scroller2');
+       		    const itemWidth = document.getElementById('block2').clientWidth;
+            if(scroller.scrollLeft < (scroller.scrollWidth - itemWidth))
+                // The scroll position is not at the beginning of last item
+                scroller.scrollBy({left: itemWidth, top: 0, behavior:'smooth'});
+            else
+                // Last item reached. Go back to first item by setting scroll position to 0
+                scroller.scrollTo({left: 0, top: 0, behavior:'smooth'});
+        });
+
+$('#cursorNext3').click(function scrollToNextItem() {
+				const scroller = document.getElementById('scroller3');
+       		    const itemWidth = document.getElementById('block3').clientWidth;
+
+            if(scroller.scrollLeft < (scroller.scrollWidth - itemWidth))
+            	
+                // The scroll position is not at the beginning of last item
+                scroller.scrollBy({left: itemWidth, top: 0, behavior:'smooth'});
+            else
+                // Last item reached. Go back to first item by setting scroll position to 0
+                scroller.scrollTo({left: 0, top: 0, behavior:'smooth'});
+        });
+
+$('#cursorPrev3').click(function scrollToNextItem() {
+				const scroller = document.getElementById('scroller3');
+       		    const itemWidth = document.getElementById('block3').clientWidth;
+            if(scroller.scrollLeft < (scroller.scrollWidth - itemWidth))
+                // The scroll position is not at the beginning of last item
+                scroller.scrollBy({left: itemWidth, top: 0, behavior:'smooth'});
+            else
+                // Last item reached. Go back to first item by setting scroll position to 0
+                scroller.scrollTo({left: 0, top: 0, behavior:'smooth'});
+        });
+
+
+      
+
+
+
